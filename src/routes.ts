af491bdd2f48fc3a094054901ddf9b77a8e976b6
Rodrigo <rodrigorael53@gmail.com>
2022-04-21 18:58:39 -0300

    criando o controller e o route da category

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
+import { CreateCategoryController } from './controllers/CreateCategoryController';
 import { CreateProductController } from './controllers/CreateProductController';
 
 const router = Router();
 
 const createProduct = new CreateProductController();
+const createCategory = new CreateCategoryController();
 
 router.post('/product', createProduct.handle);
+router.post('/category', createCategory.handle);
 
 export { router };
